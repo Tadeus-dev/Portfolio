eaae1d525131e5edfe7e919874ef2d30519e1cb9
Tadeus-dev <139873174+Tadeus-dev@users.noreply.github.com>
2026-06-19 15:47:42 -0300

    Atualização geral dos arquivos
    Adicionando novos comentários nos arquivos e realizando pequenas correções.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+//Implementação das funções de mudança de tema e alerta do formulário de contato.
 
 function mudar_tema() {
-    // O toggle liga/desliga sozinho retornando um boolean
+    // O toggle liga/desliga sozinho retornando um boolean. Cada elemento recebe a classe "dark-theme", com isso podemos configurar no css as preferências.
     const isDark = document.body.classList.toggle("dark-theme");
     
-    // Para manter o controle da variável global:
+    // Para manter o controle da variável global (Não foi necessário)
     tema_dark = isDark; 
 }
 
@@ -13,11 +14,11 @@ function exibirAlerta() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
+
+    //Condicional que verifica se qualquer um dos campos não forem preenchidos.
     if (name === "" || email === "" || message === "") {
         alert("Por favor, preencha todos os campos do formulário.");
         return; // Impede o envio do formulário
     }
-    alert("Mensagem enviada com sucesso!");
+    alert("Mensagem enviada com sucesso!"); //Só será acionado se todos os campos estiverem corretos
 }
-
-// Função para alternar o menu em telas menores
